26c77c8bfc3bd44786038d7aca93467c18e5d359
Luca Balduzzi <luca.balduzzi@nimbusintelligence.com>
2023-10-03 10:20:30 +0200

    Added new folder for modules with error.py file

---
+++ modules/errors.py
@@ -0,0 +1,8 @@
+class IpCantBeResolved(Exception):
+    
+    def __init__(self, f, *args):
+        super().__init__(args)
+        self.f = f
+
+    def __str__(self):
+        return f'The {self.f} is not in a valid range {self.min_f, self.max_f}'
