a8dc90a844efe50febe53ce26639c5f6f52f5d65
Luca Balduzzi <luca.balduzzi@nimbusintelligence.com>
2023-10-03 18:16:26 +0200

    Small fixes

--- modules/ipstack_api.py
+++ modules/ipstack_api.py
@@ -6,7 +6,7 @@ import secret_stuff
 from errors import *
 
 
-def resolve_ip(ip: str):
+def resolve_ip(ip: str) -> str:
     # IP Stack API Key
     API_KEY = secret_stuff.IP_STACK_API_KEY
 
@@ -16,17 +16,16 @@ def resolve_ip(ip: str):
     # Check if HTTP request succeeded
     if response.status_code == 200:      
         data = response.json()
-
+        print(data)
         # Check if the Ip has been resolved
         if 'location' in data and data['location']['languages'] is not None:
             return data['location']['languages'][0]['code']
         else:
-            raise IpCantBeResolved(ip) 
+            raise IpCantBeResolved(ip)
     else:
         raise ConnectionError()
 
 # Testing purposes
 if __name__ == '__main__':
-    address = input('Insert IP address: ')
-    print(resolve_ip(address))
-       
+    ip = input('Inser IP address: ')
+    print(resolve_ip(ip))       
