225ebc5ec2d5b15ec133b210bcc5ccb9eec1c762
Luca Balduzzi <luca.balduzzi@nimbusintelligence.com>
2023-10-03 15:34:32 +0200

    Added new KEY handling and ip unresolved

--- modules/ipstack_api.py
+++ modules/ipstack_api.py
@@ -1,28 +1,32 @@
+# Modules
 import requests
-import json
-import os
+
+# Custom Modules
+import secret_stuff
+from errors import *
 
 
-def resolve_ip(address):
-    #IP Stack API Key
-    API_KEY = os.getenv('IPSTACK_APIKEY')
+def resolve_ip(ip: str):
+    # IP Stack API Key
+    API_KEY = secret_stuff.IP_STACK_API_KEY
 
-    #Retrieve geolocation from IP stack
-    data = requests.get(f"http://api.ipstack.com/{address}?access_key={API_KEY}")
+    # Retrieve geolocation from IP stack
+    response = requests.get(f"http://api.ipstack.com/{ip}?access_key={API_KEY}")
     
-    #Check if HTTP request succeeded
-    if data.status_code == 200:      
-        datajson = data.json()
-        
-        #Check if a language was resolved
-        if datajson['location']['languages'] is not None:
-        
-            #Return language code
-            return datajson['location']['languages'][0]['name']
-            
+    # Check if HTTP request succeeded
+    if response.status_code == 200:      
+        data = response.json()
+
+        # Check if the Ip has been resolved
+        if 'location' in data:
+            return data['location']['languages'][0]['name']
         else:
-            raise Exception("No language was resolved")
-    
+            raise IpCantBeResolved(ip) 
     else:
-        raise Exception("Connection error")
+        raise ConnectionError()
+
+# Testing purposes
+if __name__ == '__main__':
+    address = input('Inser IP address: ')
+    print(resolve_ip(address))
        
