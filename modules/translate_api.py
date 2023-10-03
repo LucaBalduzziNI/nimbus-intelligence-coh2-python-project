541d60d45d10bce519ad8e94ca9767aa834b4962
Chris <chris.verweij@nimbusintelligence.com>
2023-10-03 16:28:47 +0200

    Some work in progress on the translate API, it does not yet run

---
+++ modules/translate_api.py
@@ -0,0 +1,23 @@
+import requests
+import json
+import os
+
+import secret_stuff
+
+def translate_string(original_string, language_code):
+    
+    #Check if language is available within the API
+    url = "https://google-translate1.p.rapidapi.com/language/translate/v2/languages"
+
+    headers = {
+        "Accept-Encoding": "application/gzip",
+        "X-RapidAPI-Key": secret_stuff.GOOGLE_TRANS_API_KEY,
+        "X-RapidAPI-Host": "google-translate1.p.rapidapi.com"
+    }
+
+    response = requests.get(url, headers=headers)
+
+    print(response.content)
+    #print(response.json())
+
+translate_string("This is a test", "nl")
