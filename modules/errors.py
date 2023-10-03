63f79dfa2eba0a3259bd78acec3c1cec8515263d
Luca Balduzzi <luca.balduzzi@nimbusintelligence.com>
2023-10-03 10:32:11 +0200

    Added first custom exceptions

--- modules/errors.py
+++ modules/errors.py
@@ -1,8 +1,29 @@
 class IpCantBeResolved(Exception):
+    """Exception raised if the given ip is does not resolve to any existing one"""
     
-    def __init__(self, f, *args):
+    def __init__(self, ip: str, *args):
         super().__init__(args)
-        self.f = f
+        self.ip = ip
 
     def __str__(self):
-        return f'The {self.f} is not in a valid range {self.min_f, self.max_f}'
+        return f'The ip {self.ip} does not resolve to any existing one!'
+    
+class LanguageCantBeTranslated(Exception):
+    """Exception raised if the given language can't be translated"""
+
+    def __init__(self, language: str, *args):
+        super().__init__(args)
+        self.language = language
+    
+    def __str__(self):
+        return f'Language {self.language} can\'t be translated!'
+
+class LanguageCantBeSpoken(Exception):
+    """Exception raised if the given language can't be spoken"""
+    
+    def __init__(self, language: str, *args):
+        super().__init__(args)
+        self.language = language
+    
+    def __str__(self):
+        return f'Language {self.language} can\'t be spoken!' 
