8ff903a878cebd45a107bd79eb2c13d789f48e58
Chris <chris.verweij@nimbusintelligence.com>
2023-10-04 16:11:26 +0200

    Table structure revised

--- documentation/table_definitions.sql
+++ documentation/table_definitions.sql
@@ -1,30 +1,36 @@
-CREATE TABLE Countries(
+CREATE OR REPLACE TABLE Countries(
     country_code varchar,
     country_name varchar,
-    country_flag varchar,
-    language_code varchar
+    country_flag varchar
 );
 
-CREATE TABLE IP_Addresses(
+CREATE OR REPLACE TABLE IP_Addresses(
     ip_address varchar,
+    country_code varchar,
+    pref_lang_code varchar
+);
+
+CREATE OR REPLACE TABLE Languages_Country(
+    language_code varchar,
     country_code varchar
 );
 
-CREATE TABLE Languages(
+CREATE OR REPLACE TABLE Languages(
     language_code varchar,
     can_be_translated boolean,
     can_be_spoken boolean
 );
 
-CREATE TABLE Text_types(
+CREATE OR REPLACE TABLE Text_types(
     text_id int NOT NULL AUTOINCREMENT,
     source_text varchar,
-    language_code varchar
+    source_lang_code varchar
 );
 
-CREATE TABLE Translations(
+CREATE OR REPLACE TABLE Translations(
     language_code varchar,
     text_id int,
     target_txt varchar,
     target_audio varchar
 );
+
