d4c7d709e529df7ebf75ace171033898e1e7e510
Chris <chris.verweij@nimbusintelligence.com>
2023-10-04 11:16:15 +0200

    Added IP Retriever

--- core_test.py
+++ core_test.py
@@ -1,9 +1,10 @@
 import modules.ipstack_api as ipstack
 import modules.translate_api as translate
 import modules.tts_api as tts
+import modules.ipify_api as ipify
 import modules.errors
 
-userIP = input("Please enter your IP ")
+userIP = ipify.get_ip()
 
 userLanguage = ipstack.resolve_ip(userIP)
 userMessage = translate.translate_string("Hello, this is a test, will this be in the correct language?", userLanguage)
