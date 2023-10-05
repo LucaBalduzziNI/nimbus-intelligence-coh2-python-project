d4706bd0f2c635c780406eace9c50643a1b9495f
Luca Balduzzi <luca.balduzzi@nimbusintelligence.com>
2023-10-05 10:48:06 +0200

    Changed return type from list[tuple] to list[dict]

--- modules/connector.py
+++ modules/connector.py
@@ -8,14 +8,14 @@ try:
 except Exception as e:
     from . import secret_stuff
 
-def execute_query(query: str, params: Tuple[str] = None) -> List[tuple]:
+def execute_query(query: str, params: Tuple[str] = None) -> List[dict]:
     """This method interacts with snowflake through the snowflake connector, handling the opening and closing of the connection.
 
     Raises:
         e: _description_
 
     Returns:
-        List[tuple]: The result of the query. It can be data (if a select statement is given) or informations about the given command. 
+        List[dict]: The result of the query. It can be data (if a select statement is given) or informations about the given command. 
     """
     # Creating the connector and using it
     with connector.connect(
@@ -28,7 +28,7 @@ def execute_query(query: str, params: Tuple[str] = None) -> List[tuple]:
         ) as conn:
         # Catching any exception to return to the caller
         try:
-            cur = conn.cursor()
+            cur = conn.cursor(connector.DictCursor)
             return cur.execute(query, params).fetchall()
         except Exception as e:
             raise e
