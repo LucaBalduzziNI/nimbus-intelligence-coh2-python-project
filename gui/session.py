627d622728444c922f067e20c8493f63764be6ec
Luca Balduzzi <luca.balduzzi@nimbusintelligence.com>
2023-10-09 16:02:15 +0200

    Added insertion error state

--- gui/session.py
+++ gui/session.py
@@ -7,6 +7,9 @@ SESSION_COUNTRY_FLAG = 'COUNTRY_FLAG'
 SESSION_COUNTRY_LANGUAGES = 'COUNTRY_LANGUAGES'
 SESSION_PREF_LANG = 'PREF_LANG'
 SESSION_PREF_LANG_SET = 'PREF_LANG_SET'
+SESSION_INS_SOURCE_TEXT = 'INS_SOURCE_TEXT'
+SESSION_INS_SOURCE_LANG = 'INS_SOURCE_LANG'
+SESSION_INS_ERROR = 'INS_ERROR'
 
 def _set_default(key: str, value: any = None):
     if key not in st.session_state.keys():
@@ -19,3 +22,6 @@ def initialize_session():
     _set_default(SESSION_COUNTRY_LANGUAGES)
     _set_default(SESSION_PREF_LANG)
     _set_default(SESSION_PREF_LANG_SET, False)
+    _set_default(SESSION_INS_SOURCE_TEXT)
+    _set_default(SESSION_INS_SOURCE_LANG)
+    _set_default(SESSION_INS_ERROR)
